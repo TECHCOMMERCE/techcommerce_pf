1bb54d871994f28ca941849d8f328f6b06d147b0
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-01-15 14:50:53 -0600

    inicializando proyecto

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+server.listen(process.env.PORT||3001/* 5000 */,  () => {
+  conn.sync({ force:  false }).then(() => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  })
+});
---
+++ api/src/routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+//router.use('/','Hola');
+
+module.exports = router;
