7577854c2e12171a098cf97f0463d81ccaf204d7
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-01-17 13:01:54 -0600

    Definiendo categorias a usar APIMercadoLibre

---
+++ api/src/controllers/Products/GetProductsAPI.js
@@ -0,0 +1,17 @@
+const { Category, Brand, Product }  = require()
+let idCategories = [
+  {
+    "id": "MLA1051",
+    "name": "Celulares y Teléfonos"
+  },
+  {
+    "id": "MLA1648",
+    "name": "Computación"
+  },
+  {
+    "id": "MLA1144",
+    "name": "Consolas y Videojuegos"
+  }
+];
+
+let limit = 200;
