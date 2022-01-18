536f60502da7b40306fe2f1816b690c098307e96
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-01-17 18:47:37 -0600

    optimizando rutas

---
+++ api/src/routes/products.js
@@ -0,0 +1,10 @@
+
+const { Router } = require('express');
+const {createNewProduct} = require('../controllers/Products/PostProduct.js')
+const router = Router();
+
+
+
+router.post('/', createNewProduct)
+
+module.exports = router;
