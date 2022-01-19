c50cb96e45ab49d5090de519db3fb17019153b6e
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 10:43:51 -0600

    adding line 3 and 9 into file:routes/category.js

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const createNewCategory = require('../controllers/Categories/PostCategories.js')
+const updateCategory = require('../controllers/Categories/UpdateCategory')
 
 const router = Router();
 
-router.post('/', createNewCategory)
+router.post('/', createNewCategory);
+
+router.put('/:id', updateCategory);
 
 module.exports = router;
