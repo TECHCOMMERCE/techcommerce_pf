d14853fdd90b314680178a681913cbf30b290414
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 16:00:44 -0600

    feature: get categories working lines added 4 and 8

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const createNewCategory = require('../controllers/Categories/PostCategories.js')
 const updateCategory = require('../controllers/Categories/UpdateCategory')
+const getCategories = require('../controllers/Categories/GetCategories')
 
 const router = Router();
+
+router.get('/', getCategories)
 
 router.post('/', createNewCategory);
 
