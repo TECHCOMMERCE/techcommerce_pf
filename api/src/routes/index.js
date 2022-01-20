7d746e7ec7ec6b6c2b670973a08c4be09532eb8f
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 21:01:59 -0600

    feature: adding line 19 is about the path brand into: routes/indes.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const products = require('./products');
 const categories = require('./helpers/category')
+const brand = require('./helpers/brand')
 
 //importing the routes
 const productPostingRoute = require('../controllers/Products/PostProduct.js')
@@ -14,4 +15,7 @@ router.use('/products', products)
 //router.use('/', productPostingRoute)
 
 router.use('/categories', categories)
+
+router.use('/brand', brand)
+
 module.exports = router;
