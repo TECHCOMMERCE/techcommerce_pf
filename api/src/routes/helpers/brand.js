37117c0dd5e89f2b726af911019c6d9aa67a5994
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 22:03:06 -0600

    adding lines where deleting functions are into: routes/helpers/brand.js;

--- api/src/routes/helpers/brand.js
+++ api/src/routes/helpers/brand.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const createNewBrand = require('../../controllers/Brand/CreateBrand');
 const updateBrand = require('../../controllers/Brand/UpdateBrand')
 const getBrands = require('../../controllers/Brand/GetBrands')
+const deleteBrand = require('../../controllers/Brand/DeleteBrand')
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.get('/', getBrands)
 router.post('/', createNewBrand);
 
 router.put('/:id', updateBrand)
+
+router.delete('/:id', deleteBrand)
 
 module.exports = router;
