56206758a05da44397e9f9969f9eeded12a895b1
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 21:51:28 -0600

    feature: adding the line 4 and 12 its about get into routes/helpers/brand.js

--- api/src/routes/helpers/brand.js
+++ api/src/routes/helpers/brand.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const createNewBrand = require('../../controllers/Brand/CreateBrand');
 const updateBrand = require('../../controllers/Brand/UpdateBrand')
-
+const getBrands = require('../../controllers/Brand/GetBrands')
 
 const router = Router();
 
 router.get('/', getBrands)
 
 router.post('/', createNewBrand);
+
+router.put('/:id', updateBrand)
 
 module.exports = router;
