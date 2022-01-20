3c21e714e203d11bb8b311fae22a8a028239500b
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-20 11:34:17 -0600

    adding new route review

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const products = require('./products');
 const categories = require('./helpers/category')
 const brand = require('./helpers/brand')
-
+const review = require('./helpers/review')
 //importing the routes
 const productPostingRoute = require('../controllers/Products/PostProduct.js')
 
@@ -17,5 +17,7 @@ router.use('/products', products)
 router.use('/categories', categories)
 
 router.use('/brand', brand)
+
+router.use('/review', review)
 
 module.exports = router;
