8741f96b0a5357424ffaad8237b22fa645a96651
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 18:19:02 -0600

    feature:line 5 and 15 adding delete feate into: routes/category.js

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const createNewCategory = require('../controllers/Categories/PostCategories.js')
 const updateCategory = require('../controllers/Categories/UpdateCategory')
 const getCategories = require('../controllers/Categories/GetCategories')
+const deleteCategory = require('../controllers/Categories/DeleteCategory')
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.get('/', getCategories)
 router.post('/', createNewCategory);
 
 router.put('/:id', updateCategory);
+
+router.delete('/:id', deleteCategory)
 
 module.exports = router;
