c25446947f18802aa9b014425a190124b84d79e2
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-19 21:48:21 -0600

    feature: adding line 3 and 10 its about update brand  into routes/helpers/brand.js

--- api/src/routes/helpers/brand.js
+++ api/src/routes/helpers/brand.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const createNewBrand = require('../../controllers/Brand/CreateBrand');
+const updateBrand = require('../../controllers/Brand/UpdateBrand')
+
 
 const router = Router();
+
+router.get('/', getBrands)
 
 router.post('/', createNewBrand);
 
