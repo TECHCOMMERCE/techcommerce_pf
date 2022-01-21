b5fda1d75fe07dce715e95a0bf2bd25a46737ce7
Tomas Pereletegui <tpereletegui@hotmail.com>
2022-01-21 14:26:32 -0300

    fixed

--- api/src/controllers/Products/getProducts.js
+++ api/src/controllers/Products/getProducts.js
@@ -2,7 +2,7 @@ const { Product, Category, Brand } = require("../../db.js");
 
 const getProducts = async (page) => {
   try {
-
+    if(page > -1) {  
     return await Product.findAll({
       
       include: [
@@ -18,7 +18,23 @@ const getProducts = async (page) => {
       ],
       limit: 9,
       offset: page * 9,
-    });
+    })}
+    else {
+      return await Product.findAll({
+      
+        include: [
+          {
+            model: Category,
+            through: {
+              attributes: [],
+            },
+          },
+          {
+            model: Brand,
+          },
+        ]
+      });
+    }
   } catch (error) {
     console.log(error);
   }
