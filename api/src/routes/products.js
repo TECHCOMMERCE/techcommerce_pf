f94675d220808c7ca1e4010b78414b3a937a69f7
Pablo <pablobustos609@gmail.com>
2022-01-26 06:18:45 -0300

    searchBar funcionando con un autocomplete

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
+const { Op } = require("sequelize");
 const {getProductsFiltered, getProducts} = require("../controllers/Products/GetProducts");
 const getProductsByQuery = require("../controllers/Products/GetProductsByQuery");
+const { Product } = require("../db");
 const router = Router();
 
 // Get de todos los productos o de los productos similares por query
@@ -27,9 +29,46 @@ router.get("/", async (req, res) => {
   return products ? res.send(products) : res.send([]);
 });
 
+router.get("/names", async (req, res) => {
+  const name = req.query.name;
+  
+  try{
+    if(name){
+      const dataTable = await Product.findAll({
+        attributes: ["name", "productid"]
+      });
+
+      let names = dataTable.map(data => {
+        return{
+          id: data.dataValues.productid,
+          name: data.dataValues.name
+        }
+      })
+      names = names.filter(productName => productName.name.toLowerCase().includes(name.toLowerCase()));
+
+      if(names.length > 0){
+        return res.send({code: 0, names});
+      }else{
+        return res.status(200).send({code: 1, names});
+      }
+    }else{
+      return res.send({code: null, results:[]})
+    }
+  }catch(e){
+    console.log(e)
+    return res.status(404).send({
+      code: 1,
+      message: "hubo un error",
+      error: e
+    })
+  }
+});
+
+
 router.get('/all', async(req, res)=> {
-  const {page} =req.query;
-  console.log('body',req.query)
+  const {page} = req.query;
+  console.log(req.query);
+  // console.log(req.query)
   const products = await getProductsFiltered(req.query, page);
   return products ? res.json(products) : res.send([]);
 })
