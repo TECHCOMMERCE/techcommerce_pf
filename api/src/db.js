aa9fad7b0964e3b1f10f5e94fad2f9d58e3b7176
Fernando Barrios <52672886+FernandoLBA@users.noreply.github.com>
2022-02-01 23:04:17 -0500

    Subiendo paginación y searchbar de productos lista

--- api/src/db.js
+++ api/src/db.js
@@ -5,8 +5,8 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-const sequelize = new Sequelize("postgres://atqdfmie:X5sVLOPrhEuL65zp63SXfzuKD1NvfpTb@kesavan.db.elephantsql.com/atqdfmie", {
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false,  //set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
