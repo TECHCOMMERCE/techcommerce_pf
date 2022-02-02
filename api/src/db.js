3c7fcbe07dbf597b8adb35c1a41d41fe3a9f8b23
Fernando Barrios <52672886+FernandoLBA@users.noreply.github.com>
2022-02-02 13:46:19 -0500

    Subiendo cambios con nodemon en producción

--- api/src/db.js
+++ api/src/db.js
@@ -6,11 +6,13 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
+// PARA LA BASE DE DATOS LOCAL
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
 //   logging: false,  //set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
+// PARA LA BASE DE DATOS EN HEROKU
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
