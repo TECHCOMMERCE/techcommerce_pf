b2a987831fe8dda6f9b33f0af8b1e0bf204ae925
Fernando Barrios <52672886+FernandoLBA@users.noreply.github.com>
2022-02-03 13:12:55 -0500

    Puto archivo categories

--- api/src/controllers/Categories/getCategories.js
+++ api/src/controllers/Categories/getCategories.js
@@ -1,38 +1,16 @@
-const { Op } = require("sequelize");
 const { Category } = require("../../db");
 
-const getCategories = async (query) => {
+const getCategories = async (name) => {
   try {
-    if (query.name && query.page) {
-      const count = await Category.count();
-      let limiter = Math.floor(count / 10);
-      return await Category.findAndCountAll({
-        where: { name: { [Op.iLike]: `%${query.name}%` } },
-        order: ["name"],
-        limit: 10,
-        offset: (query.page <= limiter && query.page) * 10,
-      });
-    }
-
-    if (query.page) {
-      const count = await Category.count();
-      let limiter = Math.floor(count / 10);
-      return await Category.findAll({
-        order: ["name"],
-        limit: 10,
-        offset: (query.page <= limiter && query.page) * 10,
-      });
-    }
-
-    if (query.name) {
+    if (name) {
       return await Category.findAll({
         where: {
-          name: { [Op.iLike]: `%${query.name}%` },
+          name: name,
         },
         order: [["name", "ASC"]],
       });
     }
-
+    
     return await Category.findAll({
       order: [["name", "ASC"]],
     });
