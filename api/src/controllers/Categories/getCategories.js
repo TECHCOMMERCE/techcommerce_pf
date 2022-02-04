ee1550d0efe5172e8e73487f1b7f716a04287b98
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-02-04 11:55:42 -0600

    getcategories add

---
+++ api/src/controllers/Categories/getCategories.js
@@ -0,0 +1,44 @@
+const { Category } = require("../../db");
+
+const getCategories = async (name) => {
+  try {
+
+    if (query.name && query.page) {
+      const count = await Category.count();
+      let limiter = Math.floor(count / 10);
+      return await Category.findAndCountAll({
+        where: { name: { [Op.iLike]: `%${query.name}%` } },
+        order: ["name"],
+        limit: 10,
+        offset: (query.page <= limiter && query.page) * 10,
+      });
+    }
+
+    if (query.page) {
+      const count = await Category.count();
+      let limiter = Math.floor(count / 10);
+      return await Category.findAll({
+        order: ["name"],
+        limit: 10,
+        offset: (query.page <= limiter && query.page) * 10,
+      });
+    }
+
+    if (query.name) {
+      return await Category.findAll({
+        where: {
+          name: name,
+        },
+        order: [["name", "ASC"]],
+      });
+    }
+    
+    return await Category.findAll({
+      order: [["name", "ASC"]],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = getCategories;
