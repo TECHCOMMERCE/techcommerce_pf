fbf60f8f27666ba239690e62a235920a19e2cf70
Jorge Martin Galliano <martin.galliano@hotmail.com>
2022-02-06 18:41:29 -0300

    css

--- client/src/components/ProductDet/index.jsx
+++ client/src/components/ProductDet/index.jsx
@@ -73,7 +73,6 @@ const Product = () => {
 				<div className={s.cont_prin}>
 					<Row>
 						<Col xs={12} md={12} lg={8} className={s.cont_img}>
-
 							<img src={product.image} alt="product" style={{height: '40%', width: '15%'}}></img>
 						</Col>
 						<Col xs={12} md={12} lg={4} className={s.cont_info}>
